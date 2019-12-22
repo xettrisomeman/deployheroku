3d100aba945225d6e64c0109e56bef3094735947
webizfabulous <webizmoxx@gmail.com>
2019-12-22 17:43:11 +0545

    added all files

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     #localapp
     'project.apps.ProjectConfig',
 ]
-
+AUTH_USER_MODEL = 'project.CustomUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newapp',
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
+        'HOST':'',
+        'PORT':''
     }
 }
 
--- app/project/models.py
+++ app/project/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    pass
+
+
+
